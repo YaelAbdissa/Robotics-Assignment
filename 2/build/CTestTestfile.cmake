257db95195928e28e8fa27a7de37cd08b7d576e2
Eyael <eyaelabdissa8@gmai.com>
2021-05-24 14:00:54 +0300

    first commit

---
+++ 2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yael/Robotics/assig_2/src
+# Build directory: /home/yael/Robotics/assig_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arm_description")
---
+++ 2/build/arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yael/Robotics/assig_2/src/arm_description
+# Build directory: /home/yael/Robotics/assig_2/build/arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
